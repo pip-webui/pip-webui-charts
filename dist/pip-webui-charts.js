ee9e98539b255e88d42b56b6cf6cb7b51c938a61
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-13 21:11:19 -0500

    Rebuilt with new version of angular material

---
+++ dist/pip-webui-charts.js
@@ -0,0 +1,18 @@
+/**
+ * @file Registration of chart WebUI controls
+ * @copyright Digital Living Software Corp. 2014-2016
+ */
+
+/* global angular */
+
+(function (angular) {
+    'use strict';
+
+    angular.module('pipCharts', [
+//        'pipMarkdown'
+    ]);
+
+})(window.angular);
+
+
+//# sourceMappingURL=pip-webui-charts.js.map
