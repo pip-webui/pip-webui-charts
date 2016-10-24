53161e6cbf47b3e0a778fe9a5da04d8a9e5e2b6f
dvoikinalex <94dvoikin@gmail.com>
2016-10-24 23:06:55 +0300

    update line chart

--- src/line/line_chart.js
+++ src/line/line_chart.js
@@ -100,6 +100,15 @@
                     chartElem = d3.select($element.get(0)).select('.line-chart svg');
                     chartElem.datum(vm.data).style('height', 270).call(chart);
 
+                    addZoom({
+                        xAxis  : chart.xAxis,
+                        yAxis  : chart.yAxis,
+                        yDomain: chart.yDomain,
+                        xDomain: chart.xDomain,
+                        redraw : function() { chart.update(); },
+                        svg    : chartElem
+                    });
+
                     nv.utils.windowResize(chart.update);
 
                     return chart;
@@ -112,6 +121,82 @@
                         }, 200 * index);
                     });
                 });
+
+                function addZoom(options) {
+                    // scaleExtent
+                    var scaleExtent = 4;
+
+                    // parameters
+                    var yAxis       = options.yAxis;
+                    var xAxis       = options.xAxis;
+                    var xDomain     = options.xDomain || xAxis.scale().domain;
+                    var yDomain     = options.yDomain || yAxis.scale().domain;
+                    var redraw      = options.redraw;
+                    var svg         = options.svg;
+                    var discrete    = options.discrete;
+
+                    // scales
+                    var xScale = xAxis.scale();
+                    var yScale = yAxis.scale();
+
+                    // min/max boundaries
+                    var x_boundary = xScale.domain().slice();
+                    var y_boundary = yScale.domain().slice();
+
+                    // create d3 zoom handler
+                    var d3zoom = d3.behavior.zoom();
+
+                    // ensure nice axis
+                    xScale.nice();
+                    yScale.nice();
+
+                    // fix domain
+                    function fixDomain(domain, boundary) {
+                        if (discrete) {
+                            domain[0] = parseInt(domain[0]);
+                            domain[1] = parseInt(domain[1]);
+                        }
+                        domain[0] = Math.min(Math.max(domain[0], boundary[0]), boundary[1] - boundary[1]/scaleExtent);
+                        domain[1] = Math.max(boundary[0] + boundary[1]/scaleExtent, Math.min(domain[1], boundary[1]));
+
+                        return domain;
+                    }
+
+                    // zoom event handler
+                    function zoomed() {
+                        console.log('event', d3.event);
+
+                        if (d3.event.sourceEvent.type === 'wheel') {
+                            if (d3.event.scale === 1) {
+                                unzoomed();
+                            } else {
+                                yDomain(fixDomain(yScale.domain(), y_boundary));
+                                xDomain(fixDomain(xScale.domain(), x_boundary));
+                                redraw();
+                            }
+                        } else {
+                            console.log('mousemove');
+                        }
+                    }
+
+                    // zoom event handler
+                    function unzoomed() {
+                        xDomain(x_boundary);
+                        yDomain(y_boundary);
+                        redraw();
+                        d3zoom.scale(1);
+                        d3zoom.translate([0,0]);
+                    }
+
+                    // initialize wrapper
+                    d3zoom.x(xScale)
+                        .y(yScale)
+                        .scaleExtent([1, scaleExtent])
+                        .on('zoom', zoomed);
+
+                    // add handler
+                    d3.select($element.get(0)).call(d3zoom).on('dblclick.zoom', unzoomed);
+                }
 
                 /**
                  * Converts palette color name into RGBA color representation.
